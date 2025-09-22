8651041587e8c3086f21680dabbe570b74052101
andre <andre>
2025-09-22 11:32:48 -0300

    Atualização package.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-const usersRoutes = require("./rotas/users");
-const logger = require("./middleware/logger");
+const usersRoutes = require("./routes/users");
+const logger = require("./middleware/auth");
 
 const app = express();
 app.use(express.json());
